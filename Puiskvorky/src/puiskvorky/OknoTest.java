cbeb82186e4b0647c341afb004e1fe4f8575d6e2
Kuba D <Kuba D@10.4.217.242>
2013-03-27 20:49:48 +0100

    Test vykreslovaci grafiky

---
+++ Puiskvorky/src/puiskvorky/OknoTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puiskvorky;
+
+import java.awt.BorderLayout;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Kuba D
+ */
+public class OknoTest extends JFrame{
+    PuiskvorkovniceND puis;
+    public OknoTest(){
+        super("Test");
+        this.setSize(400+17, 400+39);
+        this.setLayout(new BorderLayout());
+        this.add(puis = new PuiskvorkovniceND(10,10,400,400),BorderLayout.CENTER);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+    }
+    public static void main(String[] args) {
+        OknoTest a=new OknoTest();
+        try {
+            a.puis.pridej(5, 5, KK.KRIZEK);
+            a.puis.pridej(5, 6, KK.KOLECKO);
+            a.wait(30000);
+            a.puis.pridej(6, 6, KK.KRIZEK);
+            a.wait(30000);
+            a.puis.pridej(7, 7, KK.KOLECKO);
+            a.wait(30000);
+            a.puis.pridej(4, 6, KK.KRIZEK);
+            a.wait(30000);
+            a.puis.pridej(3, 7, KK.KOLECKO);
+            a.wait(30000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(OknoTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        a.dispose();
+        
+    }
+    
+}
